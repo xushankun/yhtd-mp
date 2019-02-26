96c3125a1dc4a50dae21eb997edddfc8c4996a26
xusk02 <xusk02@zendaimoney.com>
2019-02-26 16:48:33 +0800

    优化

---
+++ cloudfunctions/delItem/index.js
@@ -0,0 +1,13 @@
+const cloud = require('wx-server-sdk')
+cloud.init({
+    env: 'shankun-r-7ee811',     // 这里填写【环境ID】 而不是环境名
+    traceUser: true,    // 是否在将用户访问记录到用户管理中，在控制台中可见
+})
+const db = cloud.database()
+exports.main = async (event, context) => {
+    try {
+        return await db.collection('kklist').doc(event._id).remove()
+    } catch (e) {
+        console.error(e)
+    }
+}
